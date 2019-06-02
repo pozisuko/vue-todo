cdae6938983b9e25c0c98c956b2e72f171f858d4
chiaki-kato <makuragi@me.com>
2019-06-02 23:17:05 +0900

    add  "done" btn

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,18 +1,15 @@
-
 <template>
-  <v-list-tile>
+  <v-list-tile v-if="!done">
     <v-list-tile-content>{{ title }}</v-list-tile-content>
     <v-list-tile-action>
-      <div class="“text-xs-center”">
-        <v-btn outline color="“indigo”">Done</v-btn>
-        <v-btn outline color="“teal”">Delete</v-btn>
+      <div class="text-xs-center">
+        <v-btn outline color="success" @click="done=!done">Done</v-btn>
       </div>
     </v-list-tile-action>
   </v-list-tile>
 </template>
 
 <script>
-import { ELOOP } from "constants";
 // タイトルと完了したかどうかという状態を持つデータ
 export default {
   data() {
