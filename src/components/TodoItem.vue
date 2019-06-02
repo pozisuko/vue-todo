612c8bd10fb75c832be4aa3ef5b97a261ae57af3
chiaki-kato <makuragi@me.com>
2019-06-02 21:36:04 +0900

    - add delete button - add "done" in data

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,17 +1,24 @@
+
 <template>
   <v-list-tile>
     <v-list-tile-content>{{ title }}</v-list-tile-content>
     <v-list-tile-action>
-      <input type="checkbox" v-model="toggle" true-value="yes" false-value="no">
+      <div class="“text-xs-center”">
+        <v-btn outline color="“indigo”">Done</v-btn>
+        <v-btn outline color="“teal”">Delete</v-btn>
+      </div>
     </v-list-tile-action>
   </v-list-tile>
 </template>
 
 <script>
+import { ELOOP } from "constants";
+// タイトルと完了したかどうかという状態を持つデータ
 export default {
   data() {
     return {
-      toggle: "no"
+      toggle: "no",
+      done: false
     };
   },
   props: {
